03a854e81999f9276796bea432950fa24cce7201
João Pedro Schmitz <jpedroschmitz@hotmail.com>
2020-01-20 15:35:05 -0300

    chore: Adds default options

--- @rocketseat/gatsby-theme-docs/util/options.js
+++ @rocketseat/gatsby-theme-docs/util/options.js
@@ -1,5 +1,19 @@
 module.exports = themeOptions => {
-  const baseMetadata = {};
+  const baseMetadata = {
+    metadata: {
+      siteTitle: `Rocketseat Gatsby Themes`,
+      siteTitleAlt: `Rocketseat Gatsby Themes`,
+      siteTitleShort: `Rocketseat Gatsby Themes`,
+      siteDescription: `Discover free Gatsby themes, plugins and templates created with love by Rocketseat.`,
+      siteUrl: `https://gatsby-theme-rocketseat.github.io`,
+      siteAuthor: `@rocketseat`,
+      siteName: `Rocketseat Gatsby Themes`,
+      siteImage: `/banner.png`,
+      siteLanguage: `en_us`,
+      themeColor: `#7159c1`,
+      footer: `Created with <3`,
+    },
+  };
 
   const basePath = themeOptions.basePath || `/`;
   const configPath = themeOptions.configPath || `config`;
