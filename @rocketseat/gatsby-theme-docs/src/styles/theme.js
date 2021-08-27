45592019d26398e733758f3cb96b5ae8c5f4f8a1
benomatis <benomatis@gmail.com>
2021-08-27 15:46:15 +0200

    chore: added comma at the end of the line in the fonts object, body property, required by prettier

--- @rocketseat/gatsby-theme-docs/src/styles/theme.js
+++ @rocketseat/gatsby-theme-docs/src/styles/theme.js
@@ -13,6 +13,6 @@ export default {
     },
   },
   fonts: {
-    body: `-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif`
+    body: `-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif`,
   },
 };
